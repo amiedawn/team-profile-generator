6730b46573f8682a47be78f5e69ba49910544859
amiedawn <amie.d.carroll@gmail.com>
2020-11-24 17:47:34 -0700

    Employee test done

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,3 @@
-const { TestScheduler } = require('jest');
 const Employee = require('../lib/Employee');
 
 // test that Employee is an object
@@ -7,32 +6,47 @@ test('Employee returns object', () => {
   expect(typeof(empObj)).toBe('object');
 });
 
-// test if we can set name through our constructor
+// test if name can be set through the constructor
 test('value of name is returned', () => {
   const name = 'Matthews';
   const empObj = new Employee(name);
   expect(empObj.name).toBe(name);
 });
 
-// test if we can set id through our constructor
+// test if id can be set through the constructor
 test('value of id is returned', () => {
   const id = 123;
   const empObj = new Employee('Amie', id);
   expect(empObj.id).toBe(id);
 });
 
+// test if email can be set through the constructor
+test('value of email is returned', () => {
+  const email = 'bill@yahoo.com';
+  const empObj = new Employee('Amie', 345, email);
+  expect(empObj.email).toBe(email);
+});
 
-
-
-// test if getName returns a name
+// test functions
+// test if getName() returns a name
 test('return a name', () => {
   const nameValue = 'Matthews';
   const empObj = new Employee(nameValue);
   expect(empObj.getName()).toBe(nameValue);
 });
 
+// test if getId() returns an ID
+test('return an ID', () => {
+  const idValue = '789';
+  const empObj = new Employee('Amie', idValue);
+  expect(empObj.getId()).toBe(idValue);
+});
 
-
+// test if getEmail() returns an email
+test('return an email', () => {
+  const emailValue = 'liz@aol.com';
+  const empObj = new Employee('Amie', 432, emailValue);
+});
 
 // test if getRole will return Employee
 test('return Employee', () => {

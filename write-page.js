be52ebaaba0947c656ac848b71077ed6b8999ba7
amiedawn <amie.d.carroll@gmail.com>
2020-11-25 22:59:30 -0700

    reorganize file structore

---
+++ write-page.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const fs = require('fs');
+const path = require('path');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const templateFolder = 
+const team = '';
+
+// write sections per job
+// write manager
+const writeManager = manager => {
+  const template = fs.readFileSync(path.resolve())
+}
+
+
+
+
+
+
+
+
+
+
+const generatePage = (name, github) => {
+
+
+module.exports = generatePage;
+
